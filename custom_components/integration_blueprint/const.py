589323019ab4563565614b81d8b959edb3578ff1
Andrew Jackson <andrew@codechimp.org>
2023-08-17 14:52:21 +0100

    Renaming

--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,9 +1,16 @@
 """Constants for battery_types."""
+import json
 from logging import Logger, getLogger
+from pathlib import Path
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "battery_types"
-VERSION = "0.0.0"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+manifestfile = Path(__file__).parent / "manifest.json"
+with open(file=manifestfile, encoding="UTF-8") as json_file:
+    manifest_data = json.load(json_file)
+
+DOMAIN = manifest_data.get("domain")
+NAME = manifest_data.get("name")
+VERSION = manifest_data.get("version")
+ISSUEURL = manifest_data.get("issue_tracker")
+MANUFACTURER = "@Andrew-CodeChimp"
