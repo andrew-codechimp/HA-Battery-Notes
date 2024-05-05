dc438f4ba36480692bdcb218418c12e4e9078592
Andrew Jackson <andrew@codechimp.org>
2024-05-05 15:53:34 +0100

    Add entity to store

--- custom_components/battery_notes/store.py
+++ custom_components/battery_notes/store.py
@@ -26,9 +26,18 @@ SAVE_DELAY = 10
 
 @attr.s(slots=True, frozen=True)
 class DeviceEntry:
-    """Battery Notes storage Entry."""
+    """Battery Notes Device storage Entry."""
 
     device_id = attr.ib(type=str, default=None)
+    battery_last_replaced = attr.ib(type=datetime, default=None)
+    battery_last_reported = attr.ib(type=datetime, default=None)
+    battery_last_reported_level = attr.ib(type=float, default=None)
+
+@attr.s(slots=True, frozen=True)
+class EntityEntry:
+    """Battery Notes Entity storage Entry."""
+
+    entity_id = attr.ib(type=str, default=None)
     battery_last_replaced = attr.ib(type=datetime, default=None)
     battery_last_reported = attr.ib(type=datetime, default=None)
     battery_last_reported_level = attr.ib(type=float, default=None)
@@ -56,6 +65,7 @@ class BatteryNotesStorage:
         """Initialize the storage."""
         self.hass = hass
         self.devices: MutableMapping[str, DeviceEntry] = {}
+        self.entities: MutableMapping[str, EntityEntry] = {}
         self._store = MigratableStore(
             hass,
             STORAGE_VERSION_MAJOR,
@@ -67,12 +77,19 @@ class BatteryNotesStorage:
         """Load the registry of schedule entries."""
         data = await self._store.async_load()
         devices: OrderedDict[str, DeviceEntry] = OrderedDict()
+        entities: OrderedDict[str, EntityEntry] = OrderedDict()
 
         if data is not None and "devices" in data:
             for device in data["devices"]:
                 devices[device["device_id"]] = DeviceEntry(**device)
 
         self.devices = devices
+
+        if data is not None and "entities" in data:
+            for entity in data["entities"]:
+                entities[entity["entity_id"]] = EntityEntry(**entity)
+
+        self.entities = entities
 
     @callback
     def async_schedule_save(self) -> None:
@@ -89,6 +106,7 @@ class BatteryNotesStorage:
         store_data = {}
 
         store_data["devices"] = [attr.asdict(entry) for entry in self.devices.values()]
+        store_data["entities"] = [attr.asdict(entry) for entry in self.entities.values()]
 
         return store_data
 
@@ -136,6 +154,47 @@ class BatteryNotesStorage:
         """Update existing DeviceEntry."""
         old = self.devices[device_id]
         new = self.devices[device_id] = attr.evolve(old, **changes)
+        self.async_schedule_save()
+        return new
+
+    @callback
+    def async_get_entity(self, entity_id) -> DeviceEntry:
+        """Get an existing EntityEntry by id."""
+        res = self.entities.get(entity_id)
+        return attr.asdict(res) if res else None
+
+    @callback
+    def async_get_entities(self):
+        """Get an existing EntityEntry by id."""
+        res = {}
+        for key, val in self.entities.items():
+            res[key] = attr.asdict(val)
+        return res
+
+    @callback
+    def async_create_entity(self, entity_id: str, data: dict) -> EntityEntry:
+        """Create a new EntityEntry."""
+        if entity_id in self.entities:
+            return False
+        new_entity = EntityEntry(**data, entity_id=entity_id)
+        self.entities[entity_id] = new_entity
+        self.async_schedule_save()
+        return new_entity
+
+    @callback
+    def async_delete_entity(self, entity_id: str) -> None:
+        """Delete EntityEntry."""
+        if entity_id in self.entities:
+            del self.entities[entity_id]
+            self.async_schedule_save()
+            return True
+        return False
+
+    @callback
+    def async_update_entity(self, entity_id: str, changes: dict) -> EntityEntry:
+        """Update existing EntityEntry."""
+        old = self.entities[entity_id]
+        new = self.entities[entity_id] = attr.evolve(old, **changes)
         self.async_schedule_save()
         return new
 
