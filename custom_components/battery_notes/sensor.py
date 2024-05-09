f9fb2d07aa70d074f1996dd87c8d8a80cb352a11
Andrew Jackson <andrew@codechimp.org>
2024-05-09 13:37:05 +0100

    Fix sensor updates

--- custom_components/battery_notes/sensor.py
+++ custom_components/battery_notes/sensor.py
@@ -132,6 +132,8 @@ async def async_setup_entry(
     entity_registry = er.async_get(hass)
     device_registry = dr.async_get(hass)
 
+    device_id = config_entry.data.get(CONF_DEVICE_ID, None)
+
     async def async_registry_updated(event: Event) -> None:
         """Handle entity registry update."""
         data = event.data
