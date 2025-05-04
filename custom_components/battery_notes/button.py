929bcbd61f403be9384a6a1a166be5918deadf8a
Andrew Jackson <andrew@codechimp.org>
2025-05-04 14:57:17 +0100

    lint

--- custom_components/battery_notes/button.py
+++ custom_components/battery_notes/button.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 import logging
 from dataclasses import dataclass
-from datetime import datetime
 
 import voluptuous as vol
 from homeassistant.components.button import (
