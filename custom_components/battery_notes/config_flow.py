2a9450ce213f820ab00c9dd35242ebe193d18be9
Andrew Jackson <andrew@codechimp.org>
2025-08-03 09:05:08 +0000

    Tidy up old code within config_flow

--- custom_components/battery_notes/config_flow.py
+++ custom_components/battery_notes/config_flow.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import copy
 import logging
 from typing import Any
 
@@ -13,11 +12,8 @@ from homeassistant import config_entries
 from homeassistant.components.sensor.const import SensorDeviceClass
 from homeassistant.config_entries import (
     ConfigEntry,
-    ConfigEntryState,
-    ConfigFlow,
     ConfigFlowResult,
     ConfigSubentry,
-    ConfigSubentryData,
     ConfigSubentryFlow,
     OptionsFlow,
     SubentryFlowResult,
@@ -28,16 +24,9 @@ from homeassistant.const import (
     Platform,
 )
 from homeassistant.core import callback, split_entity_id
-from homeassistant.data_entry_flow import AbortFlow, FlowResultType, section
+from homeassistant.data_entry_flow import section
 from homeassistant.helpers import selector
 from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
-from homeassistant.helpers.selector import (
-    SelectSelector,
-    SelectSelectorConfig,
-    TextSelector,
-    TextSelectorConfig,
-    TextSelectorType,
-)
 from homeassistant.helpers.typing import DiscoveryInfoType
 
 from .common import get_device_model_id
@@ -403,20 +392,11 @@ class BatteryNotesFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 assert device_entry
                 title = device_entry.name_by_user or device_entry.name
 
-            # return self.async_create_entry(
-            #     title=str(title),
-            #     data=self.data,
-            # )
-
-
             config_entry = self.hass.config_entries.async_entries(domain=DOMAIN)[0]
+
             # Create a subentry
             subentry = ConfigSubentry(subentry_type="battery_note", data=self.data, title=str(title), unique_id=unique_id)
             self.hass.config_entries.async_add_subentry(config_entry, subentry)
-
-            # config_flow_result: ConfigFlowResult = await self.async_step_user(
-            #     discovered_data
-            # )
 
             return self.async_abort(reason="created_sub_entry")
 
@@ -926,195 +906,3 @@ class BatteryNotesSubentryFlowHandler(ConfigSubentryFlow):
             data_schema=data_schema,
             errors=errors,
         )
-
-#TODO: Change this to be sub entry options flow
-# class OptionsFlowHandler(OptionsFlow):
-#     """Handle an option flow for BatteryNotes."""
-
-#     model_info: ModelInfo | None = None
-
-#     def __init__(self) -> None:
-#         """Initialize options flow."""
-#         self.current_config: dict
-#         self.source_device_id: str
-#         self.name: str
-#         self.battery_type: str
-#         self.battery_quantity: int
-#         self.battery_low_template: str
-#         self.filter_outliers: bool
-
-#     async def async_step_init(
-#         self,
-#         user_input: dict[str, Any] | None = None,
-#     ) -> ConfigFlowResult:
-#         """Handle options flow."""
-#         errors = {}
-#         self.current_config = dict(self.config_entry.data)
-#         self.source_device_id = self.current_config.get(CONF_DEVICE_ID)  # type: ignore
-#         self.name = str(self.current_config.get(CONF_NAME) or "")
-#         self.battery_type = str(self.current_config.get(CONF_BATTERY_TYPE) or "")
-#         self.battery_quantity = int(self.current_config.get(CONF_BATTERY_QUANTITY) or 1)
-#         self.battery_low_template = str(
-#             self.current_config.get(CONF_BATTERY_LOW_TEMPLATE) or ""
-#         )
-#         self.filter_outliers = bool(
-#             self.current_config.get(CONF_FILTER_OUTLIERS) or False
-#         )
-
-#         if self.source_device_id:
-#             device_registry = dr.async_get(self.hass)
-#             device_entry = device_registry.async_get(self.source_device_id)
-
-#             if not device_entry:
-#                 errors["base"] = "orphaned_battery_note"
-#             else:
-#                 if device_entry and device_entry.manufacturer and device_entry.model:
-#                     _LOGGER.debug(
-#                         "Looking up device %s %s %s %s",
-#                         device_entry.manufacturer,
-#                         device_entry.model,
-#                         get_device_model_id(device_entry) or "",
-#                         device_entry.hw_version,
-#                     )
-
-#                     self.model_info = ModelInfo(
-#                         device_entry.manufacturer,
-#                         device_entry.model,
-#                         get_device_model_id(device_entry),
-#                         device_entry.hw_version,
-#                     )
-
-#         schema = self.build_options_schema()
-#         if user_input is not None:
-#             user_input[CONF_BATTERY_QUANTITY] = int(user_input[CONF_BATTERY_QUANTITY])
-#             user_input[CONF_BATTERY_LOW_THRESHOLD] = int(
-#                 user_input[CONF_BATTERY_LOW_THRESHOLD]
-#             )
-#             # user_input[CONF_BATTERY_LOW_TEMPLATE] = user_input.get(CONF_BATTERY_LOW_TEMPLATE, None)
-#             errors = await self.save_options(user_input, schema)
-#             if not errors:
-#                 return self.async_create_entry(title="", data={})
-
-#         return self.async_show_form(
-#             step_id="init",
-#             description_placeholders={
-#                 "manufacturer": self.model_info.manufacturer if self.model_info else "",
-#                 "model": self.model_info.model if self.model_info else "",
-#                 "model_id": (
-#                     str(self.model_info.model_id or "") if self.model_info else ""
-#                 ),
-#                 "hw_version": (
-#                     str(self.model_info.hw_version or "") if self.model_info else ""
-#                 ),
-#             },
-#             data_schema=schema,
-#             errors=errors,
-#         )
-
-#     async def save_options(
-#         self,
-#         user_input: dict[str, Any],
-#         schema: vol.Schema,
-#     ) -> dict:
-#         """Save options, and return errors when validation fails."""
-#         errors = {}
-
-#         device_registry = dr.async_get(self.hass)
-#         device_entry = device_registry.async_get(
-#             str(self.config_entry.data.get(CONF_DEVICE_ID))
-#         )
-
-#         source_entity_id = self.config_entry.data.get(CONF_SOURCE_ENTITY_ID, None)
-
-#         entity_entry: er.RegistryEntry | None = None
-#         if source_entity_id:
-#             entity_registry = er.async_get(self.hass)
-#             entity_entry = entity_registry.async_get(source_entity_id)
-#             if not entity_entry:
-#                 errors["base"] = "orphaned_battery_note"
-#                 return errors
-#         else:
-#             if not device_entry:
-#                 errors["base"] = "orphaned_battery_note"
-#                 return errors
-
-#         title: Any = ""
-#         if CONF_NAME in user_input:
-#             title = user_input.get(CONF_NAME)
-#         elif source_entity_id and entity_entry:
-#             title = entity_entry.name or entity_entry.original_name
-#         elif device_entry:
-#             title = device_entry.name_by_user or device_entry.name
-
-#         self._process_user_input(user_input, schema)
-#         self.hass.config_entries.async_update_entry(
-#             self.config_entry,
-#             title=title,
-#             data=self.current_config,
-#         )
-#         return {}
-
-#     def _process_user_input(
-#         self,
-#         user_input: dict[str, Any],
-#         schema: vol.Schema,
-#     ) -> None:
-#         """Process the provided user input against the schema."""
-#         for key in schema.schema:
-#             if isinstance(key, vol.Marker):
-#                 key = key.schema
-#             if key in user_input:
-#                 self.current_config[key] = user_input.get(key)
-#             elif key in self.current_config:
-#                 self.current_config.pop(key)
-
-#     def build_options_schema(self) -> vol.Schema:
-#         """Build the options schema."""
-#         data_schema = vol.Schema(
-#             {
-#                 vol.Optional(CONF_NAME): selector.TextSelector(
-#                     selector.TextSelectorConfig(type=selector.TextSelectorType.TEXT),
-#                 ),
-#                 vol.Required(CONF_BATTERY_TYPE): selector.TextSelector(
-#                     selector.TextSelectorConfig(type=selector.TextSelectorType.TEXT),
-#                 ),
-#                 vol.Required(CONF_BATTERY_QUANTITY): selector.NumberSelector(
-#                     selector.NumberSelectorConfig(
-#                         min=1, max=100, mode=selector.NumberSelectorMode.BOX
-#                     ),
-#                 ),
-#                 vol.Required(CONF_BATTERY_LOW_THRESHOLD): selector.NumberSelector(
-#                     selector.NumberSelectorConfig(
-#                         min=0, max=99, mode=selector.NumberSelectorMode.BOX
-#                     ),
-#                 ),
-#                 vol.Optional(CONF_BATTERY_LOW_TEMPLATE): selector.TemplateSelector(),
-#                 vol.Optional(CONF_FILTER_OUTLIERS): selector.BooleanSelector(),
-#             }
-#         )
-
-#         return _fill_schema_defaults(
-#             data_schema,
-#             self.current_config,
-#         )
-
-def _fill_schema_defaults(
-    data_schema: vol.Schema,
-    options: dict[str, Any],
-) -> vol.Schema:
-    """Make a copy of the schema with suggested values set to saved options."""
-    schema = {}
-    for key, val in data_schema.schema.items():
-        new_key = key
-        if key in options and isinstance(key, vol.Marker):
-            if (
-                isinstance(key, vol.Optional)
-                and callable(key.default)
-                and key.default()
-            ):
-                new_key = vol.Optional(key.schema, default=options.get(key))  # type: ignore
-            else:
-                new_key = copy.copy(key)
-                new_key.description = {"suggested_value": options.get(key)}  # type: ignore
-        schema[new_key] = val
-    return vol.Schema(schema)
