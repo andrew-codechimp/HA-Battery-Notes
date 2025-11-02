f189b702af7598e5ae7c14c3872c8a5f6b2c0184
Elliot Worth <elliotworth@protonmail.com>
2025-11-02 08:53:23 -0600

    refactor regenerate_device_list()

--- .github/scripts/library_doc/update_library_files.py
+++ .github/scripts/library_doc/update_library_files.py
@@ -33,27 +33,11 @@ def regenerate_device_list() -> None:
     """Generate static file containing the device library."""
 
     # Load the existing JSON library file
-    with open("library/library.json", encoding="UTF-8") as f:
-        devices_json = json.loads(f.read())
+    with open("library/library.json", encoding="UTF-8") as file:
+        devices_json = json.loads(file.read())
         devices = devices_json.get("devices")
 
-    toc_links: list[str] = []
-    tables_output: str = ""
     rows = []
-
-    num_devices = len(devices)
-
-    writer = MarkdownTableWriter()
-    headers = [
-        "Manufacturer",
-        "Model",
-        "Battery Type",
-        "Model ID (optional)",
-        "HW Version (optional)",
-    ]
-
-    writer.header_list = headers
-
     for device in devices:
         if device.get("battery_quantity", 1) > 1:
             battery_type_qty = f"{device['battery_quantity']}× {device['battery_type']}"
@@ -78,14 +62,25 @@ def regenerate_device_list() -> None:
         ]
         rows.append(row)
 
+    writer = MarkdownTableWriter()
+    writer.header_list = [
+        "Manufacturer",
+        "Model",
+        "Battery Type",
+        "Model ID (optional)",
+        "HW Version (optional)",
+    ]
     writer.value_matrix = rows
-    tables_output += f"## {num_devices} Devices in library\n\n"
-    tables_output += "This file is auto generated, do not modify\n\n"
-    tables_output += "Request new devices to be added to the library [here](https://github.com/andrew-codechimp/HA-Battery-Notes/issues/new?template=new_device_request.yml&title=%5BDevice%5D%3A+)\n\n"
-    tables_output += writer.dumps()
+
+    tables_output = [f"## {len(devices)} Devices in library\n\n"]
+    tables_output.append("This file is auto generated, do not modify\n\n")
+    tables_output.append(
+        "Request new devices to be added to the library [here](https://github.com/andrew-codechimp/HA-Battery-Notes/issues/new?template=new_device_request.yml&title=%5BDevice%5D%3A+)\n\n"
+    )
+    tables_output.append(writer.dumps())
 
     with open("library.md", "w", encoding="UTF-8") as md_file:
-        md_file.write("".join(toc_links) + tables_output)
+        md_file.write("".join(tables_output))
         md_file.close()
 
 
