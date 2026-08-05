25ea9c191c68839122605090d1438168e0404855
Andrew Jackson <andrew@codechimp.org>
2026-08-05 09:21:38 +0100

    Ignore integration domains

--- custom_components/battery_notes/discovery.py
+++ custom_components/battery_notes/discovery.py
@@ -125,7 +125,11 @@ class DiscoveryManager:
                 # Change to device_entry.config_entry_id when HA 2026.8 is minimum
                 config_entry_id = next(iter(device_entry.config_entries))
                 config_entry = self.hass.config_entries.async_get_entry(config_entry_id)
+
                 if config_entry:
+                    if library.is_domain_ignored(config_entry.domain):
+                        continue
+
                     integration = await async_get_integration(
                         self.hass, config_entry.domain
                     )
