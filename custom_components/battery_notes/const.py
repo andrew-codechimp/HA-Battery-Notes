5bd94d42ba53d12da02d6ccb4278329d59d1377b
Andrew Jackson <andrew@codechimp.org>
2026-08-15 12:01:38 +0100

    Update minimum Home Assistant version to 2026.3.0 in const.py and hacs.json

--- custom_components/battery_notes/const.py
+++ custom_components/battery_notes/const.py
@@ -10,7 +10,7 @@ from homeassistant.helpers import config_validation as cv
 
 LOGGER: Logger = getLogger(__package__)
 
-MIN_HA_VERSION = "2026.2.0"
+MIN_HA_VERSION = "2026.3.0"
 
 DOMAIN = "battery_notes"
 NAME = "Battery Notes"
@@ -104,33 +104,25 @@ WINDOW_SIZE_UNIT_TIME = 2
 
 ISSUE_DEPRECATED_YAML = "deprecated_yaml"
 
-SERVICE_BATTERY_REPLACED_SCHEMA = vol.Schema(
-    {
-        vol.Optional(ATTR_DEVICE_ID): cv.string,
-        vol.Optional(ATTR_SOURCE_ENTITY_ID): cv.string,
-        vol.Optional(SERVICE_DATA_DATE_TIME_REPLACED): cv.datetime,
-    }
-)
+SERVICE_BATTERY_REPLACED_SCHEMA = vol.Schema({
+    vol.Optional(ATTR_DEVICE_ID): cv.string,
+    vol.Optional(ATTR_SOURCE_ENTITY_ID): cv.string,
+    vol.Optional(SERVICE_DATA_DATE_TIME_REPLACED): cv.datetime,
+})
 
-SERVICE_CHECK_BATTERY_LAST_REPLACED_SCHEMA = vol.Schema(
-    {
-        vol.Required(SERVICE_DATA_DAYS_LAST_REPLACED): cv.positive_int,
-        vol.Optional(SERVICE_DATA_RAISE_EVENTS, default=True): cv.boolean,
-    }
-)
+SERVICE_CHECK_BATTERY_LAST_REPLACED_SCHEMA = vol.Schema({
+    vol.Required(SERVICE_DATA_DAYS_LAST_REPLACED): cv.positive_int,
+    vol.Optional(SERVICE_DATA_RAISE_EVENTS, default=True): cv.boolean,
+})
 
-SERVICE_CHECK_BATTERY_LAST_REPORTED_SCHEMA = vol.Schema(
-    {
-        vol.Required(SERVICE_DATA_DAYS_LAST_REPORTED): cv.positive_int,
-        vol.Optional(SERVICE_DATA_RAISE_EVENTS, default=True): cv.boolean,
-    }
-)
+SERVICE_CHECK_BATTERY_LAST_REPORTED_SCHEMA = vol.Schema({
+    vol.Required(SERVICE_DATA_DAYS_LAST_REPORTED): cv.positive_int,
+    vol.Optional(SERVICE_DATA_RAISE_EVENTS, default=True): cv.boolean,
+})
 
-SERVICE_CHECK_BATTERY_LOW_SCHEMA = vol.Schema(
-    {
-        vol.Optional(SERVICE_DATA_RAISE_EVENTS, default=True): cv.boolean,
-    }
-)
+SERVICE_CHECK_BATTERY_LOW_SCHEMA = vol.Schema({
+    vol.Optional(SERVICE_DATA_RAISE_EVENTS, default=True): cv.boolean,
+})
 
 PLATFORMS: Final = [
     Platform.BINARY_SENSOR,
