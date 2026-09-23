4d68c4b62fa4f0ba3619aa7e088d5a86afc74462
p7lejo <117734130+p7lejo@users.noreply.github.com>
2026-09-23 13:32:00 +0200

    Add battery replacement count storage key

--- custom_components/battery_notes/const.py
+++ custom_components/battery_notes/const.py
@@ -18,6 +18,7 @@ MANUFACTURER = "@Andrew-CodeChimp"
 LAST_REPLACED = "battery_last_replaced"
 LAST_REPORTED = "battery_last_reported"
 LAST_REPORTED_LEVEL = "battery_last_reported_level"
+BATTERY_REPLACEMENT_COUNT = "battery_replacement_count"
 
 DEFAULT_BATTERY_LOW_THRESHOLD = 10
 DEFAULT_BATTERY_INCREASE_THRESHOLD = 25
